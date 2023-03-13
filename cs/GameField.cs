0bc9d7eae8d762a68da4f9bb4050b3b3800a63ae
tarczaldj <tarczaldj@GYOMLAPTOP>
2023-03-13 14:03:54 +0100

    következő tetü init

--- cs/GameField.cs
+++ cs/GameField.cs
@@ -17,13 +17,18 @@ public partial class GameField : ColorRect {
 	float delay = 1000;
 	Manager gameManager = new();
 	Node2D gameCanvas;
+    Node2D nextView;
 	ColorRect[,] shapeControls;
+	ColorRect[,] nextShapeControls;
     public override async void _Ready() {
 		gameCanvas = GetNode<Node2D>("MainField/GameCanvas");
+        nextView = GetNode<Node2D>("Scoreboard/ColorRect/NextView");
 		shapeControls = ConstructGameField(gameManager.grid);
-		await GameLoop();
+        nextShapeControls = ConstructNextShapeField();
+        await GameLoop();
 	}
 
+    //public override void _Process(double delta) {
     public override void _Input(InputEvent @event) {
         bool rotatePressed = Input.IsActionJustPressed("rotate");
         bool leftPressed = Input.IsActionPressed("left");
@@ -40,7 +45,7 @@ public partial class GameField : ColorRect {
 
     private ColorRect[,] ConstructGameField(Grid grid) {
 		shapeControls = new ColorRect[grid.Rows, grid.Columns];
-		int cellSize = 20;
+		int cellSize = 40;
 		int padding = 0;
 
 		for (int rows = 0; rows < grid.Rows; rows++) {
@@ -57,7 +62,26 @@ public partial class GameField : ColorRect {
 		return shapeControls;
 	}
 
-	private void DrawGrid(Grid grid) {
+    private ColorRect[,] ConstructNextShapeField() {
+        nextShapeControls = new ColorRect[4, 4];
+        int cellSize = 20;
+        int padding = 0;
+
+        for (int rows = 0; rows < 4; rows++) {
+            for (int cols = 0; cols < 4; cols++) {
+                ColorRect shapeControl = new();
+                shapeControl.Color = shapeColors[0];
+                shapeControl.Size = new Vector2(cellSize, cellSize);
+                shapeControl.Position = new Vector2(cols * cellSize + padding, (rows - 2) * cellSize + padding);
+                nextView.AddChild(shapeControl);
+                nextShapeControls[rows, cols] = shapeControl;
+            }
+        }
+
+        return nextShapeControls;
+    }
+
+    private void DrawGrid(Grid grid) {
 		for (int rows = 0; rows < grid.Rows; rows++) {
 			for (int cols = 0; cols < grid.Columns; cols++) {
 				int id = grid[rows, cols];
@@ -76,9 +100,19 @@ public partial class GameField : ColorRect {
 		}
 	}
 
+    private void DrawNext(Shape nextShape) {
+		
+        for (int row = 0; row < nextShape.CurrentRows; row++) {
+            for (int col = 0; col < nextShape.CurrentCols; col++) {
+                nextShapeControls[row, col].Color = shapeColors[nextShape.Id];
+            }
+        }
+    }
+
 	private void Draw(Manager gameManager) {
 		DrawGrid(gameManager.grid);
 		DrawShapes(gameManager.ActiveShape);
+		DrawNext(gameManager.queue.NextShape);
 		Label lineText = GetNode<Label>("Lines/LineText");
 		lineText.Text = $"LINES: {gameManager.Score:0000}";
 	}
