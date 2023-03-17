166e4e11dacd77407d1eab6c8f68f80b8c44e673
tarczaldj <tarczaldj@GYOMLAPTOP>
2023-03-17 10:51:13 +0100

    Gombok működnek

--- cs/Menu.cs
+++ cs/Menu.cs
@@ -1,20 +1,46 @@
 using Godot;
-using System;
-using System.Linq;
 
 public partial class Menu : Control {
-    public string selected;
+    public string Selected { get; private set; }
 
     public override void _Ready() {
         Godot.Collections.Array<Node> gyerekek = GetChildren();
         foreach (Node n in gyerekek) {
-            if (n.SceneFilePath.Split('/').Last()[0] == 'S' && n is Button btn) {
-                btn.Pressed += () => Button_Pressed(btn.SceneFilePath.Split('/').Last()[6..]);
+            if (n.Name.ToString()[0] == 'S' && n is Button btn) {
+                GD.Print($"\t{n.Name.ToString()[0]}");
+                btn.Pressed += () => DiffButton_Pressed(n.Name.ToString()[6..]);
             }
         }
+
+        GetNode<Button>("Play").Pressed += Play_Pressed;
+
+        UpdateColors();
     }
 
-    private void Button_Pressed(string start) {
-        GD.Print(start);
+    void UpdateColors() {
+        bool selectionExists = false;
+
+        foreach (Node n in GetChildren()) {
+            if (n.Name.ToString()[0] == 'S' && n is Button btn) {
+                if (btn.Name == $"Select{Selected}") {
+                    selectionExists = true;
+                    btn.Modulate = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                }
+                else {
+                    btn.Modulate = new Color(1.0f, 1.0f, 1.0f, 0.75f);
+                }
+            }
+        }
+
+        GetNode<Button>("Play").Disabled = !selectionExists;
+    }
+
+    private void DiffButton_Pressed(string start) {
+        Selected = start;
+        UpdateColors();
+    }
+
+    private void Play_Pressed() {
+        GD.Print($"jac {Selected}");
     }
 }
