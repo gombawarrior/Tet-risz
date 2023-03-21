54c78f1ce56523ce3ca6282a5bb53dbde9e8b885
tarczaldj <tarczaldj@GYOMLAPTOP>
2023-03-21 14:04:39 +0100

    inputkey kezelés

--- cs/GUI/ControlMenu.cs
+++ cs/GUI/ControlMenu.cs
@@ -6,38 +6,36 @@ using System.Linq;
 public partial class ControlMenu : Control {
     private string _action;
     private Control _popup;
-    private Godot.Collections.Array<StringName> _actionArray;
+    //private Godot.Collections.Array<StringName> _actionArray;
     public static ControlMenu Instance;
 
     public override void _Ready() {
         Instance = this;
 
         _popup = GetNode<Control>("PopupMenu");
-        _actionArray = InputMap.GetActions();
+        //_actionArray = InputMap.GetActions();
         
         foreach (Node n in GetChildren()) {
             if (n.Name.ToString()[0] == 'B' && n is Button btn) {
                 string action = btn.Name.ToString()[6..];
                 var eventKey = (InputEventKey)InputMap.ActionGetEvents(action)[0];
                 btn.Text = eventKey.Keycode.ToString();
-                btn.Pressed += () => ControlButton_Pressed(_actionArray.First(e => e == action), eventKey.Keycode.ToString());
+                btn.Pressed += () => ControlButton_Pressed(action, eventKey);
                 
             }
         }
         
     }
 
-    private void ControlButton_Pressed(string action, string actionKey) {
+    private void ControlButton_Pressed(string action, InputEventKey actionKey) {
         _action = action;
         PopMenu.Instance.Action = _action;
         PopMenu.Instance.Key = actionKey;
-        GD.Print(_action);
-        GD.Print(actionKey);
         _popup.Visible = true;
     }
 
-    public void Ok_Pressed(string input) {
+    public void Ok_Pressed(InputEventKey input) {
         _popup.Visible = false;
-
+        
     }
 }
--- cs/GUI/PopMenu.cs
+++ cs/GUI/PopMenu.cs
@@ -2,9 +2,11 @@ using Godot;
 using System;
 
 public partial class PopMenu : Control {
-    public string Action, Key = "";
+    public string Action;
+    public InputEventKey Key;
     private Label _actionLabel;
     private Label _inputLabel;
+    private Control _popMenu;
     public static PopMenu Instance;
 
     public override void _Ready() {
@@ -13,16 +15,21 @@ public partial class PopMenu : Control {
         Button okButton = GetNode<Button>("OK");
          _actionLabel = GetNode<Label>("ActionLabel");
          _inputLabel = GetNode<Label>("InputLabel");
+         _popMenu = GetNode<Control>(".");
 
-         //okButton.Pressed += () => ControlMenu.Instance.Ok_Pressed(Key);
+         okButton.Pressed += () => ControlMenu.Instance.Ok_Pressed(Key);
+         _popMenu.VisibilityChanged += Visibility_Changed;
     }
 
     public override void _Input(InputEvent @event) {
-        
+        if (@event is InputEventKey key) {
+            Key = key;
+            _inputLabel.Text = Key.Keycode.ToString();
+        }
     }
 
     public void Visibility_Changed() {
         _actionLabel.Text = Action;
-        _inputLabel.Text = Key;
+        _inputLabel.Text = Key.Keycode.ToString();
     }
 }
