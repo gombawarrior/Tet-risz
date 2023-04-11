8e8f0f7a982c5168d90c3d24cd78f5a5021baa7e
tarczaldj <tarczaldj@protonmail.com>
2023-04-11 21:49:53 +0200

    még

--- Tet-Risz/cs/GUI/GameField.cs
+++ Tet-Risz/cs/GUI/GameField.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Tetrisz;
 
 public partial class GameField : ColorRect {
-    private Color[] _shapeColors = {
+	private Color[] _shapeColors = {
 		new("black", 0.0f),
 		new("cyan"),
 		new("yellow"),
@@ -15,66 +15,66 @@ public partial class GameField : ColorRect {
 		new("purple")
 	};
 
-    private bool _gameRunning, _isHard = false;
-    private float _minDelay, _maxDelay, _delayDecrease;
-    private double _nextMove = 0.085;
-    private Manager _gameManager = new();
-    private Node2D _gameCanvas, _nextView;
-    private ColorRect[,] _shapeControls, _nextShapeControls;
-    private Label _lineText;
-    private Menu _menu;
-    private PauseMenu _pauseMenu;
-    public static GameField Instance;
+	private bool _gameRunning, _isHard = false;
+	private float _minDelay, _maxDelay, _delayDecrease;
+	private double _nextMove = 0.085;
+	private Manager _gameManager = new();
+	private Node2D _gameCanvas, _nextView;
+	private ColorRect[,] _shapeControls, _nextShapeControls;
+	private Label _lineText;
+	private Menu _menu;
+	private PauseMenu _pauseMenu;
+	public static GameField Instance;
 
-    public override void _Ready() {
-        Instance = this;
+	public override void _Ready() {
+		Instance = this;
 
-        _lineText = GetNode<Label>("Lines/LineText");
-        _gameCanvas = GetNode<Node2D>("MainField/GameCanvas");
-        _nextView = GetNode<Node2D>("Scoreboard/ColorRect/NextView");
-        _menu = GetNode<Menu>("Menus/MainMenu");
-        _pauseMenu = GetNode<PauseMenu>("Menus/PauseMenu");
+		_lineText = GetNode<Label>("Lines/LineText");
+		_gameCanvas = GetNode<Node2D>("MainField/GameCanvas");
+		_nextView = GetNode<Node2D>("Scoreboard/ColorRect/NextView");
+		_menu = GetNode<Menu>("Menus/MainMenu");
+		_pauseMenu = GetNode<PauseMenu>("Menus/PauseMenu");
 		_shapeControls = ConstructGameField(_gameManager.Grid);
-        _nextShapeControls = ConstructNextShapeField();
+		_nextShapeControls = ConstructNextShapeField();
 
-        _pauseMenu._Init();
-    }
+		_pauseMenu._Init();
+	}
 
-    public override void _Process(double delta) {
-        if (!_gameRunning) return;
+	public override void _Process(double delta) {
+		if (!_gameRunning) return;
 
-        _nextMove -= delta;
+		_nextMove -= delta;
 
-        if (_nextMove > 0) return;
-        bool rotatePressed = Input.IsActionJustPressed("Rotate");
-        bool leftPressed = Input.IsActionPressed("Left");
-        bool rightPressed = Input.IsActionPressed("Right");
-        bool downPressed = Input.IsActionPressed("Down");
-        bool dropPressed = Input.IsActionPressed("Drop");
-        bool pausePressed = Input.IsActionPressed("Pause");
+		if (_nextMove > 0) return;
+		bool rotatePressed = Input.IsActionJustPressed("Rotate");
+		bool leftPressed = Input.IsActionPressed("Left");
+		bool rightPressed = Input.IsActionPressed("Right");
+		bool downPressed = Input.IsActionPressed("Down");
+		bool dropPressed = Input.IsActionPressed("Drop");
+		bool pausePressed = Input.IsActionPressed("Pause");
 
-        if (rotatePressed) _gameManager.RotateShape();
-        if (leftPressed) _gameManager.MoveLeft();
-        if (rightPressed) _gameManager.MoveRight();
-        if (downPressed) _gameManager.MoveDown();
-        if (dropPressed) _gameManager.DropShape();
+		if (rotatePressed) _gameManager.RotateShape();
+		if (leftPressed) _gameManager.MoveLeft();
+		if (rightPressed) _gameManager.MoveRight();
+		if (downPressed) _gameManager.MoveDown();
+		if (dropPressed) _gameManager.DropShape();
 
-        if (rotatePressed || leftPressed || rightPressed || downPressed) {
-            _nextMove = 0.085;
-        }
+		if (rotatePressed || leftPressed || rightPressed || downPressed) {
+			_nextMove = 0.085;
+		}
 
-        if (dropPressed) {
-            _nextMove = 0.3;
-        }
+		if (dropPressed) {
+			_nextMove = 0.3;
+		}
 
-        Draw(_gameManager);
+		Draw(_gameManager);
 
-        if (pausePressed) {
-            PauseInput_Pressed();
-        }
-    }
+		if (pausePressed) {
+			PauseInput_Pressed();
+		}
+	}
 
-    private ColorRect[,] ConstructGameField(Grid grid) {
+	private ColorRect[,] ConstructGameField(Grid grid) {
 		_shapeControls = new ColorRect[grid.Rows, grid.Columns];
 		const int cellSize = 40;
 
@@ -92,25 +92,25 @@ public partial class GameField : ColorRect {
 		return _shapeControls;
 	}
 
-    private ColorRect[,] ConstructNextShapeField() {
-        _nextShapeControls = new ColorRect[4, 4];
-        const int cellSize = 20;
+	private ColorRect[,] ConstructNextShapeField() {
+		_nextShapeControls = new ColorRect[4, 4];
+		const int cellSize = 20;
 
-        for (int rows = 0; rows < 4; rows++) {
-            for (int cols = 0; cols < 4; cols++) {
-                ColorRect shapeControl = new();
-                shapeControl.Color = _shapeColors[0];
-                shapeControl.Size = new Vector2(cellSize, cellSize);
-                shapeControl.Position = new Vector2(cols * cellSize, rows * cellSize);
-                _nextView.AddChild(shapeControl);
-                _nextShapeControls[rows, cols] = shapeControl;
-            }
-        }
+		for (int rows = 0; rows < 4; rows++) {
+			for (int cols = 0; cols < 4; cols++) {
+				ColorRect shapeControl = new();
+				shapeControl.Color = _shapeColors[0];
+				shapeControl.Size = new Vector2(cellSize, cellSize);
+				shapeControl.Position = new Vector2(cols * cellSize, rows * cellSize);
+				_nextView.AddChild(shapeControl);
+				_nextShapeControls[rows, cols] = shapeControl;
+			}
+		}
 
-        return _nextShapeControls;
-    }
+		return _nextShapeControls;
+	}
 
-    private void DrawGrid(Grid grid) {
+	private void DrawGrid(Grid grid) {
 		for (int rows = 0; rows < grid.Rows; rows++) {
 			for (int cols = 0; cols < grid.Columns; cols++) {
 				int id = grid[rows, cols];
@@ -129,106 +129,106 @@ public partial class GameField : ColorRect {
 		}
 	}
 
-    private void DrawNext(Shape nextShape) {
+	private void DrawNext(Shape nextShape) {
 		// clean nextView
-        for (int row = 0; row < _nextShapeControls.GetLength(0); row++) {
-            for (int col = 0; col < _nextShapeControls.GetLength(1); col++) {
-                _nextShapeControls[row, col].Color = _shapeColors[0];
-            }
-        }
+		for (int row = 0; row < _nextShapeControls.GetLength(0); row++) {
+			for (int col = 0; col < _nextShapeControls.GetLength(1); col++) {
+				_nextShapeControls[row, col].Color = _shapeColors[0];
+			}
+		}
 
-        for (int row = 0; row < nextShape.CurrentRows; row++) {
-            for (int col = 0; col < nextShape.CurrentCols; col++) {
-                if (nextShape.CurrentShapeMatrix[row, col] == 1) {
-                    _nextShapeControls[row, col].Color = _shapeColors[nextShape.Id];
-                }
-            }
-        }
-    }
+		for (int row = 0; row < nextShape.CurrentRows; row++) {
+			for (int col = 0; col < nextShape.CurrentCols; col++) {
+				if (nextShape.CurrentShapeMatrix[row, col] == 1) {
+					_nextShapeControls[row, col].Color = _shapeColors[nextShape.Id];
+				}
+			}
+		}
+	}
 
-    private void DrawGhost(Shape shape) {
-        int dropDistance = _gameManager.ShapeDropDistance();
-        for (int row = 0; row < shape.CurrentRows; row++) {
-            for (int col = 0; col < shape.CurrentCols; col++) {
-                if (shape.CurrentShapeMatrix[row, col] == 1) {
-                    Color ghostColor = _shapeColors[shape.Id];
-                    ghostColor.A = 0.25f;
-                    _shapeControls[(int)shape.Pos.X + row + dropDistance, (int)shape.Pos.Y + col].Color = ghostColor;
-                }
-            }
-        }
-    }
+	private void DrawGhost(Shape shape) {
+		int dropDistance = _gameManager.ShapeDropDistance();
+		for (int row = 0; row < shape.CurrentRows; row++) {
+			for (int col = 0; col < shape.CurrentCols; col++) {
+				if (shape.CurrentShapeMatrix[row, col] == 1) {
+					Color ghostColor = _shapeColors[shape.Id];
+					ghostColor.A = 0.25f;
+					_shapeControls[(int)shape.Pos.X + row + dropDistance, (int)shape.Pos.Y + col].Color = ghostColor;
+				}
+			}
+		}
+	}
 
 	private new void Draw(Manager gameManager) {
 		DrawGrid(gameManager.Grid);
-        if (!_isHard) DrawGhost(gameManager.ActiveShape);
-        DrawShape(gameManager.ActiveShape);
+		if (!_isHard) DrawGhost(gameManager.ActiveShape);
+		DrawShape(gameManager.ActiveShape);
 		DrawNext(gameManager.Queue.NextShape);
 		_lineText.Text = $"SOROK: {gameManager.Score:0000}";
 	}
 
 	private async Task GameLoop() {
-        do {
-            Draw(_gameManager);
-            float delay = Math.Max(_minDelay, _maxDelay - (_gameManager.Score * _delayDecrease));
+		do {
+			Draw(_gameManager);
+			float delay = Math.Max(_minDelay, _maxDelay - (_gameManager.Score * _delayDecrease));
 
-            await Task.Delay(TimeSpan.FromMilliseconds(delay));
+			await Task.Delay(TimeSpan.FromMilliseconds(delay));
 
-            if (!_gameRunning) continue;
+			if (!_gameRunning) continue;
 
 			_gameManager.MoveDown();
-        } while (!_gameManager.IsGameOver && _gameRunning);
+		} while (!_gameManager.IsGameOver && _gameRunning);
 
-        if (!_gameManager.IsGameOver) return;
+		if (!_gameManager.IsGameOver) return;
 
-        _menu.Visible = true;
-    }
+		_menu.Visible = true;
+	}
 
-    public async void Play_Pressed(string difficulty) {
-        _menu.Visible = false;
-        _gameRunning = true;
-        switch (difficulty) {
-            case "Weak":
-                _minDelay = 250;
-                _maxDelay = 1000;
-                _delayDecrease = 20;
-                break;
-            case "Med":
-                _minDelay = 175;
-                _maxDelay = 850;
-                _delayDecrease = 30;
-                break;
-            case "Hard":
-                _minDelay = 100;
-                _maxDelay = 650;
-                _delayDecrease = 50;
-                _isHard = true;
-                break;
-        }
+	public async void Play_Pressed(string difficulty) {
+		_menu.Visible = false;
+		_gameRunning = true;
+		switch (difficulty) {
+			case "Weak":
+				_minDelay = 250;
+				_maxDelay = 1000;
+				_delayDecrease = 20;
+				break;
+			case "Med":
+				_minDelay = 175;
+				_maxDelay = 850;
+				_delayDecrease = 30;
+				break;
+			case "Hard":
+				_minDelay = 100;
+				_maxDelay = 650;
+				_delayDecrease = 50;
+				_isHard = true;
+				break;
+		}
 
-        _gameManager = new Manager();
+		_gameManager = new Manager();
 
-        await GameLoop();
-    }
+		await GameLoop();
+	}
 
-    private void PauseInput_Pressed() {
-        _pauseMenu.Visible = true;
-        _gameRunning = false;
-    }
+	private void PauseInput_Pressed() {
+		_pauseMenu.Visible = true;
+		_gameRunning = false;
+	}
 
-    public async void PauseContinue_Pressed() {
-        _pauseMenu.Visible = false;
-        _gameRunning = true;
-        await GameLoop();
-    }
+	public async void PauseContinue_Pressed() {
+		_pauseMenu.Visible = false;
+		_gameRunning = true;
+		await GameLoop();
+	}
 
-    public void PauseReplay_Pressed() {
-        _pauseMenu.Visible = false;
-        _gameRunning = false;
-        _menu.Visible = true;
-    }
+	public void PauseReplay_Pressed() {
+		_pauseMenu.Visible = false;
+		_gameRunning = false;
+		_menu.Visible = true;
+	}
 
-    public void PauseExit_Pressed() {
-        GetTree().Quit();
-    }
+	public void PauseExit_Pressed() {
+		GetTree().Quit();
+	}
 }
